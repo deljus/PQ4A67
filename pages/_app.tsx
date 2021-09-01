86de4b1b2a6d802a44122a1446e76995194a97ec
deljuse <musindelus@gmail.com>
2021-09-01 16:18:54 +0300

    Fixed app import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { QueryClient, QueryClientProvider } from "react-query"
 
-import { UserContextProvider } from "@/context/user"
 import { MessageProvider } from "@/context/message"
+import { UserContextProvider } from "@/context/user"
 
 const queryClient = new QueryClient()
 
