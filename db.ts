8996cbb920a6e2ca2a5afc088dda32dc63d806d7
deljuse <musindelus@gmail.com>
2021-09-01 13:53:39 +0300

    Add deploy

--- db.ts
+++ db.ts
@@ -5,9 +5,6 @@ declare global {
 }
 
 export const prisma =
-    global.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    })
+    global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
